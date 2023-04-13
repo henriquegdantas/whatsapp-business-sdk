31066b169f92d6b9ff5d7adf57a35f70dfde50bd
Henrique Dantas <henriquegomesdantas@gmail.com>
2023-04-13 00:16:40 -0300

    :label: Creating union type for Webhook messages. This way we can branch different properties according to type value

--- src/types/webhooks.ts
+++ src/types/webhooks.ts
@@ -81,44 +81,28 @@ export type WebhookMedia = {
 	 */
 	id: string;
 };
-/**
- * For more information about this object, go here https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/components#messages-object
- */
-export type WebhookMessage = {
+
+export type WebhookMessageBase = {
 	/**
 	 * The type of message that has been received by the business that has subscribed to Webhooks.
 	 */
 	type: MessageType | "system" | "unknown";
+
+	/**
+	 * The number who sent the message
+	 */
+	from: string;
+
+	/**
+	 * The ID for the message that was received by the business. You could use messages endpoint to mark it as read.
+	 */
+	id: number;
+
 	/**
 	 * The time when the customer sent the message to the business in unix format
 	 */
 	timestamp: number;
-	/**
-	 * When the messages type is set to audio, including voice messages, this object is included in the messages object:
-	 */
-	audio?: {
-		id: string;
-		mime_type: string;
-	};
-	/**
-	 * When the messages type field is set to button, this object is included in the messages object:
-	 */
-	button?: {
-		/**
-		 * The payload for a button set up by the business that a customer clicked as part of an interactive message
-		 */
-		payload: any;
-		text: string;
-	};
-	/**
-	 * When the messages type field is set to contacts, this object is included in the messages object:
-	 */
-	contacts?: WebhookContactCard[];
 
-	/**
-	 * When the messages type field is set to location, this object is included in the messages object:
-	 */
-	location?: WebhookLocation[];
 	/**
 	 * When the messages type field is set to button, this object is included in the messages object. The context for a message that was forwarded or in an inbound reply from the customer.
 	 */
@@ -140,66 +124,7 @@ export type WebhookMessage = {
 		 */
 		id: string;
 	};
-	/**
-	 * When messages type is set to document.
-	 */
-	document?: WebhookMedia;
-	/**
-	 * The message that a business received from a customer is not a supported type.
-	 */
-	errors?: WebhookError[];
-	from: string;
-	/**
-	 * The ID for the message that was received by the business. You could use messages endpoint to mark it as read.
-	 */
-	id: number;
-	/**
-	 * A webhook is triggered when a customer's phone number or profile information has been updated.
-	 */
-	identity?: {
-		acknowledged: boolean;
-		customer_identity_changed: boolean;
-		/**
-		 * The time when the WhatsApp Business Management API detected the customer may have changed their profile information
-		 */
-		created_timestamp: string;
-		/**
-		 * The ID for the messages system customer_identity_changed
-		 */
-		hash: string;
-	};
-	/**
-	 * When messages type is set to image, this object is included in the messages object.
-	 * */
-	image?: WebhookMedia;
-	/**
-	 * When a customer selected a button or list reply.
-	 */
-	interactive?: {
-		type: {
-			/**
-			 * Sent when a customer clicks a button
-			 */
-			button_reply?: {
-				/**
-				 *  Unique ID of a button
-				 */
-				id: string;
-				title: string;
-			};
-			/**
-			 *  Sent when a customer selects an item from a list
-			 */
-			list_reply?: {
-				/**
-				 * Unique ID of the selected list item
-				 */
-				id: string;
-				title: string;
-				description: string;
-			};
-		};
-	};
+
 	/**
 	 * A customer clicked an ad that redirects them to WhatsApp.
 	 */
@@ -235,6 +160,154 @@ export type WebhookMessage = {
 		 */
 		thumbnail_url: string;
 	};
+
+	/**
+	 * A webhook is triggered when a customer's phone number or profile information has been updated.
+	 */
+	identity?: {
+		acknowledged: boolean;
+		customer_identity_changed: boolean;
+		/**
+		 * The time when the WhatsApp Business Management API detected the customer may have changed their profile information
+		 */
+		created_timestamp: string;
+		/**
+		 * The ID for the messages system customer_identity_changed
+		 */
+		hash: string;
+	};
+
+	/**
+	 * The message that a business received from a customer is not a supported type.
+	 */
+	errors?: WebhookError[];
+};
+
+export interface WebhookMessageAudio extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "audio";
+
+	/**
+	 * When the messages type is set to audio, including voice messages, this object is included in the messages object:
+	 */
+	audio?: {
+		id: string;
+		mime_type: string;
+		sha256: string;
+		voice?: boolean;
+	};
+}
+
+export interface WebhookMessageContact extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "contacts";
+
+	/**
+	 * When the messages type field is set to contacts, this object is included in the messages object:
+	 */
+	contacts?: WebhookContactCard[];
+}
+
+export interface WebhookMessageLocation extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "location";
+
+	/**
+	 * When the messages type field is set to location, this object is included in the messages object:
+	 */
+	location?: WebhookLocation[];
+}
+
+export interface WebhookMessageButton extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "button";
+
+	/**
+	 * When the messages type field is set to button, this object is included in the messages object:
+	 */
+	button?: {
+		/**
+		 * The payload for a button set up by the business that a customer clicked as part of an interactive message
+		 */
+		payload: any;
+		text: string;
+	};
+}
+
+export interface WebhookMessageDocument extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "document";
+
+	/**
+	 * When messages type is set to document.
+	 */
+	document?: WebhookMedia;
+}
+
+export interface WebhookMessageImage extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "image";
+
+	/**
+	 * When messages type is set to image, this object is included in the messages object.
+	 * */
+	image?: WebhookMedia;
+}
+
+export interface WebhookMessageInteractive extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "interactive";
+
+	/**
+	 * When a customer selected a button or list reply.
+	 */
+	interactive?: {
+		type: {
+			/**
+			 * Sent when a customer clicks a button
+			 */
+			button_reply?: {
+				/**
+				 *  Unique ID of a button
+				 */
+				id: string;
+				title: string;
+			};
+			/**
+			 *  Sent when a customer selects an item from a list
+			 */
+			list_reply?: {
+				/**
+				 * Unique ID of the selected list item
+				 */
+				id: string;
+				title: string;
+				description: string;
+			};
+		};
+	};
+}
+
+export interface WebhookMessageSticker extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "sticker";
+
 	/**
 	 * When messages type is set to sticker.
 	 */
@@ -246,6 +319,77 @@ export type WebhookMessage = {
 		 */
 		id: string;
 	};
+}
+
+export interface WebhookMessageText extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "text";
+
+	/**
+	 * When messages type is set to text.
+	 */
+	text?: {
+		body: string;
+	};
+}
+
+export interface WebhookMessageVideo extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "video";
+
+	/**
+	 * When messages type is set to video.
+	 */
+	video?: WebhookMedia;
+}
+
+export interface WebhookMessageReaction extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "reaction";
+
+	/**
+	 * When messages type is set to reaction.
+	 */
+	reaction?: ReactionMessage;
+}
+
+export interface WebhookMessageOrder extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "order";
+
+	catalod_id: string;
+
+	text: string;
+
+	product_items: {
+		product_retailer_id: string;
+		quantity: string;
+		item_price: string;
+		currency: string;
+	}[];
+}
+
+export interface WebhookMessageUnknown extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "unknown";
+}
+
+export interface WebhookMessageSystem extends WebhookMessageBase {
+	/**
+	 * A message of audio type.
+	 */
+	type: "system";
+
 	/**
 	 * When messages type is set to system, a customer has updated their phone number or profile information
 	 */
@@ -284,21 +428,26 @@ export type WebhookMessage = {
 		 */
 		customer: string;
 	};
-	/**
-	 * When messages type is set to text.
-	 */
-	text?: {
-		body: string;
-	};
-	/**
-	 * When messages type is set to video.
-	 */
-	video?: WebhookMedia;
-	/**
-	 * When messages type is set to reaction.
-	 */
-	reaction?: ReactionMessage;
-};
+}
+
+/**
+ * For more information about this object, go here https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/components#messages-object
+ */
+export type WebhookMessage =
+	| WebhookMessageAudio
+	| WebhookMessageContact
+	| WebhookMessageLocation
+	| WebhookMessageButton
+	| WebhookMessageDocument
+	| WebhookMessageImage
+	| WebhookMessageInteractive
+	| WebhookMessageSticker
+	| WebhookMessageText
+	| WebhookMessageVideo
+	| WebhookMessageReaction
+	| WebhookMessageSystem
+	| WebhookMessageOrder
+	| WebhookMessageUnknown;
 /**
  * For more information about this object, go here https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/components#statuses-object
  */
